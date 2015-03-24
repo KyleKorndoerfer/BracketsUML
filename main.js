9b503845c6cc40968b08d8e9bc93d0c73b879194
S. Kyle Korndoerfer <kyle.korndoerfer@gmail.com>
2015-03-23 21:37:12 -0400

    Add ability to render the preview when saving, reworked panel display logic, added debugging log statements.

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
-/*global define, $, brackets, window */
+/*global define, Mustache, $, brackets, window */
 
-/** Extension that adds support for generating PlantUML diagrams */
+/**
+ * Extension that adds support for generating PlantUML diagrams.
+ */
 define(function (require, exports, module) {
     "use strict";
 
-    var DocumentManager     = brackets.getModule("document/DocumentManager"),
+    var DEBUG				= true,
+		DocumentManager     = brackets.getModule("document/DocumentManager"),
         EditorManager       = brackets.getModule("editor/EditorManager"),
         WorkspaceManager    = brackets.getModule("view/WorkspaceManager"),
         ExtensionUtils      = brackets.getModule("utils/ExtensionUtils"),
@@ -28,7 +31,9 @@ define(function (require, exports, module) {
      * @param {string} msg - The title of the book.
      */
     function log(msg) {
-        console.log("%c[BracketsUML] " + msg, "color:blue");
+		if (DEBUG) {
+			console.log("%c[BracketsUML] " + msg, "color:blue");
+		}
     }// log(msg)
 
 
@@ -36,6 +41,8 @@ define(function (require, exports, module) {
      * Registers the PlantUML language syntax
      */
     function registerLanguage() {
+		log("BEGIN: registerLanguage()");
+
         // create a new 'plantuml' language mode in CodeMirror
         plantUmlLang.register(CodeMirror);
 
@@ -48,6 +55,8 @@ define(function (require, exports, module) {
             fileExtensions: ["plantuml", "uml", "wsd"],
             lineComment: ["'"]
         });
+
+		log("END: registerLanguage()");
     }// registerLanguage()
 
 
@@ -55,52 +64,77 @@ define(function (require, exports, module) {
      * Shows the diagram preview panel if not already shown
      */
     function showPreviewPanel() {
+		log("BEGIN: showPreviewPanel()");
+
         if (!panel.isVisible()) {
             log("Showing the preview panel");
             // TODO: refresh panel with local file content; if available
             panel.show();
         }
+
+		log("END: showPreviewPanel()");
     }// showPreviewPanel()
 
-
-    /** Hides the diagram preview panel if not already hidden */
+    /**
+	 * Hides the diagram preview panel if not already hidden.
+	 */
     function hidePreviewPanel() {
+		log("BEGIN: hidePreviewPanel()");
+
         if (panel.isVisible()) {
             log("Hiding the preview panel");
             panel.hide();
         }
+
+		log("END: hidePreviewPanel()");
     }//hidePreviewPanel()
 
-
-    /** Updates the preview panel image based on the editor contents */
+    /**
+	 * Updates the preview panel image based on the editor contents.
+	 */
     function updatePanel(editor) {
-        var text = editor.document.getText();
-		var encodedText = umlEncoder.compress(text);
+		log("BEGIN: updatePanel(editor)");
 
-        log("Straight text: " + text);
-        log("Encoded text: " + encodedText);
+		if (editor && editor.document) {
+			var encodedText = umlEncoder.compress(editor.document.getText());
+			log("Encoded text: " + encodedText);
 
-        // TODO: make a call to PlantUML REST API to get an image back
-        // TODO: save the image to disk (same file name, different extension
-        // TODO: refresh the panel to show the latest image
+			// TODO: save the image to disk (same file name, different extension
+			$("img", panel.$panel).attr("src", plantumlService + encodedText);
+
+			showPreviewPanel();
+		}
+
+		log("END: updatePanel(editor)");
     }// updatePanel(editor)
 
 
-    /** Updates the diagram preview panel in response to the document being updated. */
+    /**
+	 * Updates the diagram preview panel in response to the document being updated.
+	 */
     function handleFileSaved(jqEvent, doc) {
-        console.assert(EditorManager.getCurrentFullEditor() && EditorManager.getCurrentFullEditor().document === doc);
-        updatePanel(EditorManager.getCurrentFullEditor());
-        log("File saved");
+		log("BEGIN: handleFileSaved(jqEvent, doc)");
+
+		var currentEditor = EditorManager.getCurrentFullEditor();
+        console.assert(currentEditor && currentEditor.document === doc);
+        updatePanel(currentEditor);
+
+		log("END: handleFileSaved(jqEvent, doc)");
     }//handleFileSaved(jqEvent, doc)
 
 
-    /** Shows/hides the diagram preview panel based on the active editor */
+    /**
+	 * Handles the active document being changed by showing/hiding the diagram
+	 * preview panel based on language in the active editor.
+	 */
     function handleCurrentEditorChange(jqEvent, newFile, newPaneId, oldFile, oldPaneId) {
+		log("BEGIN: handleCurrentEditorChange(jqEvent, newFile, newPaneId, oldFile, oldPaneId)");
+
         var newEditor = EditorManager.getCurrentFullEditor();
         if (newEditor) {
             if (newEditor.document.getLanguage().getId() === "plantuml") {
                 DocumentManager.on("documentSaved", handleFileSaved);
-                showPreviewPanel();
+                updatePanel();
             } else {
                 DocumentManager.off("documentSaved", handleFileSaved);
                 hidePreviewPanel();
@@ -108,25 +142,31 @@ define(function (require, exports, module) {
         } else {
             hidePreviewPanel();
         }
+
+		log("END: handleCurrentEditorChange(jqEvent, newFile, newPaneId, oldFile, oldPaneId)");
     }// handleCurrentEditorChange()
 
 
     /** Registers and global event listeners */
     function registerEventListeners() {
+		log("BEGIN: registerEventListeners()");
+
         // listen for changes to the active editor
         MainViewManager.on("currentFileChange", handleCurrentEditorChange);
+
+		// Close panel when close button is clicked.
+		panel.$panel.on('click', '.close', hidePreviewPanel);
+
+		log("END: registerEventListeners()");
     }// registerEventListeners()
 
 
 	AppInit.appReady(function () {
         log("Initializing...");
-        registerLanguage();
-        registerEventListeners();
 
+		registerLanguage();
         panel = WorkspaceManager.createBottomPanel(BRACKETSUML_PREVIEW, $(panelTemplate), 150);
-
-		// Close panel when close button is clicked.
-		panel.$panel.on('click', '.close', hidePreviewPanel);
+		registerEventListeners();
 
         log("Initialized");
     });
