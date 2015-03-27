5710d2bf9e7a1f1a330003cf9bbc19d2285e5a58
S. Kyle Korndoerfer <kyle.korndoerfer@gmail.com>
2015-03-27 11:17:12 -0400

    Ability to save image from the preview toolbar

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ define(function (require, exports, module) {
     "use strict";
 
     var DEBUG				= true,
+
 		DocumentManager     = brackets.getModule("document/DocumentManager"),
         EditorManager       = brackets.getModule("editor/EditorManager"),
         WorkspaceManager    = brackets.getModule("view/WorkspaceManager"),
@@ -17,24 +18,37 @@ define(function (require, exports, module) {
         LanguageManager     = brackets.getModule("language/LanguageManager"),
         MainViewManager     = brackets.getModule("view/MainViewManager"),
         CodeMirror          = brackets.getModule("thirdparty/CodeMirror2/lib/codemirror"),
+		NodeDomain			= brackets.getModule("utils/NodeDomain"),
+		PrefsManager		= brackets.getModule("preferences/PreferencesManager"),
+
+		Diagram				= new NodeDomain("saveDiagram", ExtensionUtils.getModulePath(module, "node/saveDiagram")),
+
         umlEncoder          = require("./lib/umlEncoder"),
         plantUmlLang        = require("./lib/plantuml"),
+		panelTemplate       = require("text!previewPanel.html"),
 
-        plantumlService     = "http://www.plantuml.com:80/plantuml/png/",
+        plantumlService     = "http://www.plantuml.com/plantuml/png/",
         BRACKETSUML_PREVIEW = "bracketsuml.preview",
-        panelTemplate       = require("text!previewPanel.html"),
-        panel;
+        panel,
+		editor;
 
 
     /**
      * Logging with a little extra flair (ala Chrome)
-     * @param {string} msg - The title of the book.
+     * @param {string} msg The title of the book.
      */
     function log(msg) {
 		if (DEBUG) {
 			console.log("%c[BracketsUML] " + msg, "color:blue");
 		}
     }// log(msg)
+
+	/**
+	 * Logs the message to the error console.
+	 * @param {string} msg The message to log to the error console.
+	  */
+	function logError(msg) {
+	}// logError(msg)
 
 
     /**
@@ -68,7 +82,6 @@ define(function (require, exports, module) {
 
         if (!panel.isVisible()) {
             log("Showing the preview panel");
-            // TODO: refresh panel with local file content; if available
             panel.show();
         }
 
@@ -91,6 +104,7 @@ define(function (require, exports, module) {
 
     /**
 	 * Updates the preview panel image based on the editor contents.
+	 * @param {Editor} editor The active editor instance
 	 */
     function updatePanel(editor) {
 		log("BEGIN: updatePanel(editor)");
@@ -111,13 +125,18 @@ define(function (require, exports, module) {
 
     /**
 	 * Updates the diagram preview panel in response to the document being updated.
+	 * @param {Object} jqEvent The jQuery event object.
+	 * @param {Document} doc The active document.
 	 */
     function handleFileSaved(jqEvent, doc) {
 		log("BEGIN: handleFileSaved(jqEvent, doc)");
 
-		var currentEditor = EditorManager.getCurrentFullEditor();
-        console.assert(currentEditor && currentEditor.document === doc);
-        updatePanel(currentEditor);
+		//var currentEditor = EditorManager.getCurrentFullEditor();
+        //console.assert(currentEditor && currentEditor.document === doc);
+        //updatePanel(currentEditor);
+
+		console.assert(editor && editor.document === doc);
+		updatePanel(editor);
 
 		log("END: handleFileSaved(jqEvent, doc)");
     }//handleFileSaved(jqEvent, doc)
@@ -126,6 +145,11 @@ define(function (require, exports, module) {
     /**
 	 * Handles the active document being changed by showing/hiding the diagram
 	 * preview panel based on language in the active editor.
+	 * @param {Object} jqEvent The jQuery event object.
+	 * @param {File} newFile
+	 * @param {string} newPaneId
+	 * @param {File} oldFile
+	 * @param {string} oldPaneId
 	 */
     function handleCurrentEditorChange(jqEvent, newFile, newPaneId, oldFile, oldPaneId) {
 		log("BEGIN: handleCurrentEditorChange(jqEvent, newFile, newPaneId, oldFile, oldPaneId)");
@@ -134,9 +158,11 @@ define(function (require, exports, module) {
         if (newEditor) {
             if (newEditor.document.getLanguage().getId() === "plantuml") {
                 DocumentManager.on("documentSaved", handleFileSaved);
+				editor = newEditor;
                 updatePanel();
             } else {
                 DocumentManager.off("documentSaved", handleFileSaved);
+				editor = null;
                 hidePreviewPanel();
             }
         } else {
@@ -146,8 +172,36 @@ define(function (require, exports, module) {
 		log("END: handleCurrentEditorChange(jqEvent, newFile, newPaneId, oldFile, oldPaneId)");
     }// handleCurrentEditorChange()
 
+	/**
+	 * Saves the image generated from the text in the editor.
+	 * @param {Object} jqEvent The jQuery event object.
+	 */
+	function saveDiagram(jqEvent) {
+		log("BEGIN: saveDiagram()");
 
-    /** Registers and global event listeners */
+		var fullPath = editor.document.file.fullPath,
+			encodedText = umlEncoder.compress(editor.document.getText()),
+			fullUrl = plantumlService + encodedText,
+			filename;
+
+		// build the file path
+		filename = FileUtils.getDirectoryPath(fullPath);
+		filename += FileUtils.getFilenameWithoutExtension(editor.document.file.name);
+		filename += ".png";
+
+		Diagram.exec("save", fullUrl, FileUtils.convertToNativePath(filename), PrefsManager.get("proxy"))
+			.done(function () {
+				log("Diagram successfully saved");
+			}).fail(function (err) {
+				logError("Diagram was not saved properly: " + err);
+			});
+
+		log("END: saveDiagram()");
+	}// saveDiagram(jqEvent)
+
+    /**
+	 * Registers and global event listeners
+	 */
     function registerEventListeners() {
 		log("BEGIN: registerEventListeners()");
 
@@ -156,17 +210,24 @@ define(function (require, exports, module) {
 
 		// Close panel when close button is clicked.
 		panel.$panel.on('click', '.close', hidePreviewPanel);
+		// Save file when save button clicked
+		panel.$panel.on('click', '.save', saveDiagram);
 
 		log("END: registerEventListeners()");
     }// registerEventListeners()
 
 
+	/**
+	 * Initializes the extension.
+	 */
 	AppInit.appReady(function () {
         log("Initializing...");
 
 		registerLanguage();
         panel = WorkspaceManager.createBottomPanel(BRACKETSUML_PREVIEW, $(panelTemplate), 150);
 		registerEventListeners();
+
+		// FUTURE: Add icon to the toolbar on the right?
 
         log("Initialized");
     });
---
+++ node/saveDiagram.js
@@ -0,0 +1,64 @@
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
+/*global require, exports */
+
+/**
+ * Simple module that fetches the diagram from the PlantUML services and saves
+ * it to disk beside the diagram text file.
+ */
+(function () {
+	"use strict";
+
+	var http		= require("http"),
+		fs			= require("fs"),
+		URL			= require('url');
+
+	/**
+	 * @private
+	 * Handler function for the saveDiagram command.
+	 * @param {string} url The encoded url for the diagram on the server.
+	 * @param {string} filename The full path to save the file to.
+	 * @param {string} proxy The proxy server to use (optional)
+	 */
+	function cmdSaveDiagram(url, filename, proxy) {
+		var fStream = fs.createWriteStream(filename),
+			options;
+
+		if (proxy === undefined) {
+			options = url;
+		} else {
+			var parsedUrl = URL.parse(proxy);
+			options = {host: parsedUrl.hostname, port: parsedUrl.port, path: url};
+		}
+
+		http.get(options, function (response) {
+			response.pipe(fStream);
+		}).on('error', function (err) {
+			console.log("Got error: " + err.message);
+		});
+	}
+
+	/**
+	 * Initializes the Brackets domain with the saveDiagram command.
+	 * @param {DomainManager} domainManager The domain manager for the server.
+	 */
+	function init(domainManager) {
+		if (!domainManager.hasDomain("saveDiagram")) {
+			domainManager.registerDomain("saveDiagram", {major: 0, minor: 1});
+		}
+
+		domainManager.registerCommand(
+			"saveDiagram",		// domain name
+			"save",				// command name
+			cmdSaveDiagram,		// command helper function
+			false,				// this command is synchronous in Node
+			"Fetches the rendered diagram from the server and saves it to disk",
+			// paramters
+			[{name: "url", type: "string", description: "The URL of the image to fetch" },
+				{name: "filename", type: "string", description: "The full path to where to save the file"},
+				{name: "proxy", type: "string", description: "The proxy server to use (if needed; optional)"}],
+			null
+		);
+	}
+
+	exports.init = init;
+}());
